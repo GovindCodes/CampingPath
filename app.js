a17389c4b1e92bc882be4c49f433e035ee812856
Govind Kumar <govindkumar3006@gmail.com>
2020-09-12 15:52:27 +0530

    Included Show Page for Campground

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yelp_camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
+})  
 
 
 
@@ -20,34 +18,50 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', {
 const Schema = mongoose.Schema
 const campgroundSchema = new Schema({
 name : String,
-image: String
+image: String,
+description: String
 });
 
 const Campground = mongoose.model('Campground',campgroundSchema);
+// Campground.create({
+//     name: "Tso Moriri Lake, Ladakh",
+//     image: "https://cdn.pixabay.com/photo/2017/06/17/03/17/gongga-snow-mountain-2411069__480.jpg",
+//     description: "Tsomoriri Lake is the highest lake in the world and located in Ladakh. Camping here is the experience of a lifetime"
+// }, function(err, newlyCreated){
+//     if(err){
+//         console.log(err);
+//     }else{
+//          //redirect to campground get page
+//         console.log("adaded")
+//     }
+// })
 
 app.get("/", function(req, res){
    res.render("landing");
 })
 
+//INDEX ROUTE- Show all campgrunds
 app.get("/campgrounds", function(req, res){
    //Get all dbs from database
    Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         }else{
-            res.render("campground", {campgrounds: allCampgrounds});
+            res.render("index", {campgrounds: allCampgrounds});
         }
    })
     
 })
 
+//CREATE ROUTE - add new campground to db
 app.post("/campgrounds", function(req, res){
 
     // res.send("you hit post route");
     //get data from form and add to database array
     var name= req.body.name;
     var image= req.body.image;
-    var newCampground= {name: name, image:image};
+    var desc= req.body.description;
+    var newCampground= {name: name, image:image, description: desc};
     //save image to a database
 
     Campground.create(newCampground, function(err, newlyCreated){
@@ -62,8 +76,23 @@ app.post("/campgrounds", function(req, res){
    
 })
 
+//NEW ROUTE- show form to create
 app.get("/campgrounds/new", function(req, res){
     res.render('new.ejs');
+})
+
+//SHOW ROUTE- show data for campground
+app.get("/campgrounds/:id", function(req, res){
+    //find campground with provided id
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    })
+    
 })
 
 const port = 3000;
